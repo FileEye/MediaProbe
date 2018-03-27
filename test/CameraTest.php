e3468ab89a5cb5d265e90f9fd10486cc5f23ea7e
mondrake <mondrake@mondrake.org>
2018-03-27 18:17:34 +0200

    733

--- test/CameraTest.php
+++ test/CameraTest.php
@@ -14,29 +14,6 @@ use Symfony\Component\Yaml\Yaml;
  */
 class CameraTest extends ExifEyeTestCaseBase
 {
-    protected function assertIfd($expected, $ifd)
-    {
-        $this->assertInstanceOf($expected['class'], $ifd);
-
-        if (isset($expected['entries'])) {
-            $this->assertCount(count($expected['entries']), $ifd->getEntries());
-            foreach ($expected['entries'] as $test_entry => $test_entry_data) {
-                $entry = $ifd->getEntry(Spec::getTagIdByName($ifd->getType(), $test_entry));
-                $this->assertInstanceOf($test_entry_data['class'], $entry, "Block: '{$ifd->getName()}' Entry: '$test_entry'");
-                $this->assertEquals(unserialize($test_entry_data['value']), $entry->getValue(), "Block: '{$ifd->getName()}' Entry: '$test_entry'");
-                $this->assertEquals($test_entry_data['text'], $entry->getText(), "Block: '{$ifd->getName()}' Entry: '$test_entry'");
-            }
-        }
-
-        if (isset($expected['blocks'])) {
-            $this->assertCount(count($expected['blocks']), $ifd->getSubIfds());
-            foreach ($expected['blocks'] as $test_block => $test_block_data) {
-                $block = $ifd->getSubIfd(Spec::getIfdIdByType($test_block));
-                $this->assertIfd($test_block_data, $block);
-            }
-        }
-    }
-
     public function imageFileProvider()
     {
         $finder = new Finder();
@@ -69,8 +46,33 @@ class CameraTest extends ExifEyeTestCaseBase
             $this->assertIfd($test['blocks']['IFD1'], $ifd0->getNextIfd());
         }
 
+        // @todo readd testing of thumbnail correctness
+
         if (isset($test['errors']['entries'])) {
             $this->assertCount(count($test['errors']['entries']), ExifEye::getExceptions());
+        }
+    }
+
+    protected function assertIfd($expected, $ifd)
+    {
+        $this->assertInstanceOf($expected['class'], $ifd);
+
+        if (isset($expected['entries'])) {
+            $this->assertCount(count($expected['entries']), $ifd->getEntries());
+            foreach ($expected['entries'] as $test_entry => $test_entry_data) {
+                $entry = $ifd->getEntry(Spec::getTagIdByName($ifd->getType(), $test_entry));
+                $this->assertInstanceOf($test_entry_data['class'], $entry, "Block: '{$ifd->getName()}' Entry: '$test_entry'");
+                $this->assertEquals(unserialize($test_entry_data['value']), $entry->getValue(), "Block: '{$ifd->getName()}' Entry: '$test_entry'");
+                $this->assertEquals($test_entry_data['text'], $entry->getText(), "Block: '{$ifd->getName()}' Entry: '$test_entry'");
+            }
+        }
+
+        if (isset($expected['blocks'])) {
+            $this->assertCount(count($expected['blocks']), $ifd->getSubIfds());
+            foreach ($expected['blocks'] as $test_block => $test_block_data) {
+                $block = $ifd->getSubIfd(Spec::getIfdIdByType($test_block));
+                $this->assertIfd($test_block_data, $block);
+            }
         }
     }
 }
