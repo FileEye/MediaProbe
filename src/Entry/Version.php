befccab0580ca5475af703290d4be883759b7ab5
mondrake <mondrake@mondrake.org>
2018-03-31 16:43:11 +0200

    x

--- src/Entry/Version.php
+++ src/Entry/Version.php
@@ -12,34 +12,12 @@ use ExifEye\core\Format;
  */
 class Version
 {
-/*    protected $format = Format::UNDEFINED;
-    private $version;
-    protected $value = [];
-    public static function getInstanceArgumentsFromTagData($ifd_id, $tag_id, $format, $components, DataWindow $data_window, $data_offset)
-    {
-        if ($format != Format::UNDEFINED) {
-            throw new UnexpectedFormatException($ifd_id, $tag_id, $format, Format::UNDEFINED);
-        }
-        return [$data_window->getBytes($data_offset, $components) / 100];
-    }
-    public function setValue(array $data)
-    {
-        $this->version = isset($data[0]) ? $data[0] : 0.0;
-        $this->value[0] = $this->version;
-        $major = floor($this->version);
-        $minor = ($this->version - $major) * 100;
-        $strValue = sprintf('%02.0f%02.0f', $major, $minor);
-        $this->components = strlen($strValue);
-        $this->bytes = $strValue;
-    }
-    public function getValue()
-    {
-        return $this->version;
-    }
-*/
     protected static function validateVersion($value)
     {
-        //return floor($value) == $value ? $value .= '.0' : $value;
+        $version = ((int) $value) / 100;
+        $major = floor($version);
+        $minor = ($version - $major) * 100;
+        $value = sprintf('%02.0f%02.0f', $major, $minor);
         return $value;
     }
 
