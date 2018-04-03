29b7318e58e758a223fc54443c54044b46d4678a
mondrake <mondrake@mondrake.org>
2018-04-03 18:27:08 +0200

    x

--- src/Entry/IfdCopyright.php
+++ src/Entry/IfdCopyright.php
@@ -40,7 +40,7 @@ class IfdCopyright extends Ascii
     public function setValue(array $data)
     {
 dump($data);
-        $this->value = array_merge(['', ''], $data);
+        $this->value = array_replace(['', ''], $data);
 dump($this->value);
 
         if ($this->value[0] === '' && $this->value[1] !== '') {
