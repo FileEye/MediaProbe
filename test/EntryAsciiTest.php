9b56879104470b0400177402a14d3cec8a963c8c
mondrake <mondrake@mondrake.org>
2018-04-03 22:42:15 +0200

    4

--- test/EntryAsciiTest.php
+++ test/EntryAsciiTest.php
@@ -23,9 +23,9 @@ class EntryAsciiTest extends ExifEyeTestCaseBase
 
         $this->assertEquals(20, $entry->getComponents());
         $this->assertEquals(10, $entry->getValue());
-        $this->assertEquals(10, $entry->getValue(Time::UNIX_TIMESTAMP));
-        $this->assertEquals('1970:01:01 00:00:10', $entry->getValue(Time::EXIF_STRING));
-        $this->assertEquals(2440588 + 10 / 86400, $entry->getValue(Time::JULIAN_DAY_COUNT));
+        $this->assertEquals(10, $entry->getValue(['type' => Time::UNIX_TIMESTAMP]));
+        $this->assertEquals('1970:01:01 00:00:10', $entry->getValue(['type' => Time::EXIF_STRING]));
+        $this->assertEquals(2440588 + 10 / 86400, $entry->getValue(['type' => Time::JULIAN_DAY_COUNT]));
         $this->assertEquals('1970:01:01 00:00:10', $entry->toString());
 
         // Malformed Exif timestamp.
@@ -36,27 +36,27 @@ class EntryAsciiTest extends ExifEyeTestCaseBase
         // This is Jan 1st 1900 at 18:00, outside the range of a UNIX
         // timestamp:
         $this->assertFalse($entry->getValue());
-        $this->assertEquals('1900:01:01 18:00:00', $entry->getValue(Time::EXIF_STRING));
-        $this->assertEquals(2415021.75, $entry->getValue(Time::JULIAN_DAY_COUNT));
+        $this->assertEquals('1900:01:01 18:00:00', $entry->getValue(['type' => Time::EXIF_STRING]));
+        $this->assertEquals(2415021.75, $entry->getValue(['type' => Time::JULIAN_DAY_COUNT]));
 
         $entry->setValue(['0000:00:00 00:00:00', Time::EXIF_STRING]);
 
         $this->assertFalse($entry->getValue());
-        $this->assertEquals('0000:00:00 00:00:00', $entry->getValue(Time::EXIF_STRING));
-        $this->assertEquals(0, $entry->getValue(Time::JULIAN_DAY_COUNT));
+        $this->assertEquals('0000:00:00 00:00:00', $entry->getValue(['type' => Time::EXIF_STRING]));
+        $this->assertEquals(0, $entry->getValue(['type' => Time::JULIAN_DAY_COUNT]));
 
         $entry->setValue(['9999:12:31 23:59:59', Time::EXIF_STRING]);
 
         // this test will fail on 32bit machines
         $this->assertEquals(253402300799, $entry->getValue());
-        $this->assertEquals('9999:12:31 23:59:59', $entry->getValue(Time::EXIF_STRING));
-        $this->assertEquals(5373484 + 86399 / 86400, $entry->getValue(Time::JULIAN_DAY_COUNT));
+        $this->assertEquals('9999:12:31 23:59:59', $entry->getValue(['type' => Time::EXIF_STRING]));
+        $this->assertEquals(5373484 + 86399 / 86400, $entry->getValue(['type' => Time::JULIAN_DAY_COUNT]));
 
         // Check day roll-over for SF bug #1699489.
         $entry->setValue(['2007:04:23 23:30:00', Time::EXIF_STRING]);
-        $t = $entry->getValue(Time::UNIX_TIMESTAMP);
+        $t = $entry->getValue(['type' => Time::UNIX_TIMESTAMP]);
         $entry->setValue([$t + 3600]);
-        $this->assertEquals('2007:04:24 00:30:00', $entry->getValue(Time::EXIF_STRING));
+        $this->assertEquals('2007:04:24 00:30:00', $entry->getValue(['type' => Time::EXIF_STRING]));
     }
 
     public function testCopyright()
