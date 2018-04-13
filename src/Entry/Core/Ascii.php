c5999f47f03c65ff8cc5c247be08be888b844b3e
mondrake <mondrake@mondrake.org>
2018-04-13 14:34:59 +0200

    xx

--- src/Entry/Core/Ascii.php
+++ src/Entry/Core/Ascii.php
@@ -4,6 +4,7 @@ namespace ExifEye\core\Entry\Core;
 
 use ExifEye\core\DataWindow;
 use ExifEye\core\DataWindowOffsetException;
+use ExifEye\core\ExifEye;
 use ExifEye\core\Format;
 use ExifEye\core\Utility\ConvertBytes;
 
