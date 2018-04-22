5a5dbfe008cfcfc9cfed766529b5a708091c3e4f
mondrake <mondrake@mondrake.org>
2018-04-22 15:02:19 +0200

    xxx

--- test/CameraTest.php
+++ test/CameraTest.php
@@ -40,11 +40,11 @@ class CameraTest extends ExifEyeTestCaseBase
         $ifd0 = $tiff->getIfd();
 
         if (isset($test['blocks']['IFD0'])) {
-            $this->assertIfd($test['blocks']['IFD0'], $ifd0);
+            $this->assertBlock($test['blocks']['IFD0'], $ifd0);
         }
 
         if (isset($test['blocks']['IFD1'])) {
-            $this->assertIfd($test['blocks']['IFD1'], $ifd0->getNextIfd());
+            $this->assertBlock($test['blocks']['IFD1'], $ifd0->getNextIfd());
         }
 
         // @todo readd testing of thumbnail correctness
@@ -73,50 +73,61 @@ class CameraTest extends ExifEyeTestCaseBase
         }
     }
 
-    protected function assertIfd($expected, $ifd)
+    protected function assertBlock($expected, $block)
     {
-        dump([$ifd->getName(), $ifd->getElementPath()]);
-        $b = $ifd->xxGetSubBlocks();
-        foreach ($b as $type) {
-            foreach ($type as $block) {
-                $this->assertIfd([], $block);
+        $this->assertInstanceOf($expected['class'], $block);
+
+        // Check entry.
+        if (isset($expected['Entry'])) {
+            $entry = $block->getEntry();
+            $this->assertInstanceOf($expected['Entry']['class'], $entry, "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+            $this->assertEquals($expected['Entry']['components'], $entry->getComponents(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+            $this->assertEquals($expected['Entry']['format'], Format::getName($entry->getFormat()), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+            $this->assertEquals(unserialize(base64_decode($expected['Entry']['value'])), $tags[$i]->getEntry()->getValue(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+            $this->assertEquals($expected['Entry']['text'], $tags[$i]->getEntry()->toString(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+        }
+
+        foreach ($expected['blocks'] as $expected_blocks) {
+            $i = 0;
+            foreach ($expected_blocks as $expected_type => $expected_block) {
+                $this->assertBlock($expected_block, $block->xxGetSubBlockByIndex($expected_type; $i++));
             }
         }
-        if (empty($expected)) {
+/*        if (empty($expected)) {
             return;
         }
 
-        $this->assertInstanceOf($expected['class'], $ifd);
+        $this->assertInstanceOf($expected['class'], $block);
 
         if (isset($expected['blocks']['Tag'])) {
             $expected_tags = $expected['blocks']['Tag'];
-            $tags = $ifd->xxGetSubBlocks('Tag');
-            $this->assertCount(count($expected_tags), $tags, "IFD: '{$ifd->getName()}' - TAGs count");
+            $tags = $block->xxGetSubBlocks('Tag');
+            $this->assertCount(count($expected_tags), $tags, "IFD: '{$block->getName()}' - TAGs count");
             for ($i = 0; $i < count($tags); $i++) {
-                $this->assertEquals($expected_tags[$i]['id'], $tags[$i]->getId(), "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
-                $this->assertEquals($expected_tags[$i]['name'], $tags[$i]->getName(), "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertEquals($expected_tags[$i]['id'], $tags[$i]->getId(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertEquals($expected_tags[$i]['name'], $tags[$i]->getName(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
                 $entry = $tags[$i]->getEntry();
-                $this->assertInstanceOf($expected_tags[$i]['Entry']['class'], $entry, "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
-                $this->assertEquals($expected_tags[$i]['Entry']['components'], $entry->getComponents(), "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
-                $this->assertEquals($expected_tags[$i]['Entry']['format'], Format::getName($entry->getFormat()), "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
-                $this->assertEquals(unserialize(base64_decode($expected_tags[$i]['Entry']['value'])), $tags[$i]->getEntry()->getValue(), "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
-                $this->assertEquals($expected_tags[$i]['Entry']['text'], $tags[$i]->getEntry()->toString(), "Ifd: '{$ifd->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertInstanceOf($expected_tags[$i]['Entry']['class'], $entry, "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertEquals($expected_tags[$i]['Entry']['components'], $entry->getComponents(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertEquals($expected_tags[$i]['Entry']['format'], Format::getName($entry->getFormat()), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertEquals(unserialize(base64_decode($expected_tags[$i]['Entry']['value'])), $tags[$i]->getEntry()->getValue(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
+                $this->assertEquals($expected_tags[$i]['Entry']['text'], $tags[$i]->getEntry()->toString(), "Ifd: '{$block->getName()}' Tag: '{$tags[$i]->getId()}'");
             }
         }
 
 /*        if (isset($expected['blocks']['Ifd'])) {
             $expected_ifds = $expected['blocks']['Ifd'];
-            $this->assertCount(count($expected_ifds), $ifd->xxGetSubBlocks('Ifd'), "Block: '{$ifd->getName()}' - sub-blocks count");
+            $this->assertCount(count($expected_ifds), $block->xxGetSubBlocks('Ifd'), "Block: '{$block->getName()}' - sub-blocks count");
             foreach ($expected_ifds as $test_block_data) {
-                $block = $ifd->xxGetSubBlock('Ifd', $test_block_data['id']);
-foreach($ifd->xxGetSubBlocks('Ifd') as $x){
+                $block = $block->xxGetSubBlock('Ifd', $test_block_data['id']);
+foreach($block->xxGetSubBlocks('Ifd') as $x){
   $xxx[] = $x->getId();
   $xxy[] = $x->getName();
   $xxz[] = $x->getElementPath();
 }
-//dump([$ifd->getElementPath(), $test_block_data['id'], implode('-', $xxx), (bool) $block]);
-dump([$ifd->getName(), $ifd->getElementPath(), $test_block_data['id'], implode('-', $xxx), implode('-', $xxy), implode('-', $xxz), (bool) $block]);
-//                $this->assertIfd($test_block_data, $block);
+//dump([$block->getElementPath(), $test_block_data['id'], implode('-', $xxx), (bool) $block]);
+dump([$block->getName(), $block->getElementPath(), $test_block_data['id'], implode('-', $xxx), implode('-', $xxy), implode('-', $xxz), (bool) $block]);
+//                $this->assertBlock($test_block_data, $block);
             }
         }*/
     }
