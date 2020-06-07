86ac882a93bedb01df6b2497037aed630139ca20
mondrake <mondrake.org@gmail.com>
2020-06-07 15:03:02 +0200

    2

--- src/Command/DumpCommand.php
+++ src/Command/DumpCommand.php
@@ -14,6 +14,7 @@ use FileEye\MediaProbe\MediaProbe;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
