40ddd5d08731e97eaece8d2a238b2121fe3d417b
mondrake <mondrake.org@gmail.com>
2020-06-10 14:24:48 +0200

    d

--- src/Entry/WindowsString.php
+++ src/Entry/WindowsString.php
@@ -46,8 +46,12 @@ class WindowsString extends Byte
         $raw = $data[0];
 dump($raw);
         $datax = mb_convert_encoding($data[0], 'UTF-8', 'UCS-2LE');
-dump($datax);
 dump(MediaProbe::dumpHexFormatted(mb_convert_encoding($data[0], 'ASCII', 'UCS-2LE')));
+dump(MediaProbe::dumpHexFormatted(mb_convert_encoding($data[0], 'UTF-8', 'UCS-2LE')));
+dump(MediaProbe::dumpHexFormatted(mb_convert_encoding($data[0], 'UCS-4', 'UCS-2LE')));
+dump(MediaProbe::dumpHexFormatted(mb_convert_encoding($data[0], 'UTF-16', 'UCS-2LE')));
+dump(MediaProbe::dumpHexFormatted(mb_convert_encoding($data[0], 'UTF-32', 'UCS-2LE')));
+dump(MediaProbe::dumpHexFormatted(mb_convert_encoding($data[0], '8bit', 'UCS-2LE')));
         $php_string = rtrim($datax, "\0");
 dump($php_string);
         $windows_string = mb_convert_encoding($php_string, 'UCS-2LE', 'auto');
