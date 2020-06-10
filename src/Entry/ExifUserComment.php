4bf1eb40d8f109ee002fea15583bac3c0c364d1a
mondrake <mondrake.org@gmail.com>
2020-06-10 10:43:03 +0200

    ccc

--- src/Entry/ExifUserComment.php
+++ src/Entry/ExifUserComment.php
@@ -83,6 +83,6 @@ class ExifUserComment extends Undefined
      */
     public function toString(array $options = [])
     {
-        return $this->value[0];
+        return $this->value[0] ?? '';
     }
 }
