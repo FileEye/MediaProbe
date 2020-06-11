3576c1c6d87f394179f42524edf91cbef6f0d86a
mondrake <mondrake.org@gmail.com>
2020-06-11 10:44:10 +0200

    a

--- src/Entry/GPSVersionId.php
+++ src/Entry/GPSVersionId.php
@@ -12,6 +12,18 @@ class GPSVersionId extends Byte
     /**
      * {@inheritdoc}
      */
+    public function getValue(array $options = [])
+    {
+        $format = $options['format'] ?? null;
+        if ($format === 'phpExif') {
+            return $this->toBytes();
+        }
+        return parent::getValue();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function toString(array $options = [])
     {
         return implode('.', $this->getValue());
