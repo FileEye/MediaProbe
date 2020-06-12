3fc8caca7bb2c332de34377b19f5d953e6da84bd
mondrake <mondrake.org@gmail.com>
2020-06-12 19:36:03 +0200

    1

--- test/EntryUserCommentTest.php
+++ test/EntryUserCommentTest.php
@@ -10,15 +10,15 @@ class EntryUserCommentTest extends EntryTestBase
     public function testUsercomment()
     {
         $entry = new ExifUserComment($this->mockParentElement);
-        $entry->setValue([]);
+        $entry->setValue(["\x0\x0\x0\x0\x0\x0\x0\x0"]);
         $this->assertEquals(8, $entry->getComponents());
-        $this->assertEquals(['', ''], $entry->getValue());
+        $this->assertEquals('', $entry->getValue());
         $this->assertEquals('', $entry->toString());
         $this->assertEquals("\x0\x0\x0\x0\x0\x0\x0\x0", $entry->toBytes());
 
-        $entry->setValue(['Hello!', 'ASCII']);
+        $entry->setValue(["ASCII\x0\x0\x0Hello!"]);
         $this->assertEquals(14, $entry->getComponents());
-        $this->assertEquals(['Hello!', 'ASCII'], $entry->getValue());
+        $this->assertEquals('Hello!', $entry->getValue());
         $this->assertEquals("ASCII\x0\x0\x0Hello!", $entry->toBytes());
     }
 }
