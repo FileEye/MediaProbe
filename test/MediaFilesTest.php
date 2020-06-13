39622626cb34e43b9dd68774b5b4375133075a6c
mondrake <mondrake.org@gmail.com>
2020-06-13 15:17:34 +0200

    5

--- test/MediaFilesTest.php
+++ test/MediaFilesTest.php
@@ -54,7 +54,6 @@ class MediaFilesTest extends MediaProbeTestCaseBase
         if (isset($this->testDump['exiftool_raw'])) {
             $this->exiftoolRawDump =new \DOMDocument();
             $this->exiftoolRawDump->loadXML($this->testDump['exiftool_raw']);
-dump($this->exiftoolRawDump);
         }
         $media = Media::createFromFile($mediaDumpFile->getPath() . '/' . $this->testDump['fileName']);
 
@@ -165,8 +164,10 @@ dump($this->exiftoolRawDump);
             if ($exiftool_node = $element->getParentElement()->getCollection()->getPropertyValue('exiftoolDOMNode')) {
                 $exiftool_node_skip = $this->testDump['skip']['exiftool'] ?? [];
                 if (!in_array($exiftool_node, $exiftool_node_skip)) {
-                    $xml_node = $this->exiftoolRawDump->getElementsByTagName($exiftool_node);
-                    dump([$element->getContextPath(), $exiftool_node]);
+                    $XPath = new \DOMXPath($this->exiftoolRawDump);
+                    $xml_node = $XPath->query($exiftool_node);
+                    //$xml_node = $this->exiftoolRawDump->getElementsByTagName($exiftool_node);
+                    dump([$element->getContextPath(), $exiftool_node, $xml_node]);
 //                    dump([$element->getContextPath(), $element->getValue(), $xml_node]);
                     //$this->assertSame($expected_tag_value, $element->getValue(['format' => 'phpExif']), $element->getContextPath());
                 }
