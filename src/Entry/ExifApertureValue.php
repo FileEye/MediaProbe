6a9a9cad52dc2a8cea92fab8f2bfe89763f889bb
mondrake <mondrake.org@gmail.com>
2020-06-13 19:44:18 +0200

    26

--- src/Entry/ExifApertureValue.php
+++ src/Entry/ExifApertureValue.php
@@ -13,6 +13,18 @@ class ExifApertureValue extends Rational
     /**
      * {@inheritdoc}
      */
+    public function getValue(array $options = [])
+    {
+        $format = $options['format'] ?? null;
+        if ($format === 'exiftool') {
+            return pow(2, $this->getValue()[0] / $this->getValue()[1] / 2);
+        }
+        return parent::getValue($options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function toString(array $options = [])
     {
         return MediaProbe::fmt('%.01f', pow(2, $this->getValue()[0] / $this->getValue()[1] / 2));
