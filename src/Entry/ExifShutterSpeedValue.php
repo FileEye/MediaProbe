cdd3a761ae81d117c381c704c8fc8e561517cbb3
mondrake <mondrake.org@gmail.com>
2020-06-13 22:37:52 +0200

    33

--- src/Entry/ExifShutterSpeedValue.php
+++ src/Entry/ExifShutterSpeedValue.php
@@ -13,6 +13,25 @@ class ExifShutterSpeedValue extends SignedRational
     /**
      * {@inheritdoc}
      */
+    public function getValue(array $options = [])
+    {
+        $format = $options['format'] ?? null;
+        if ($format === 'phpExif') {
+            $val = $this->getValue()[0] / $this->getValue()[1];
+            $val = abs($val) < 100 ? pow(2, -$val) : 0;
+            if ($val < 0.25001 && $val > 0) {
+                return MediaProbe::fmt("1/%d", (int) (0.5 + 1 / $val));
+            } else {
+                $val = MediaProbe::fmt("%.1f", $val);
+                return preg_replace('/\.0$/', '', $val);
+            }
+        }
+        return parent::getValue();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function toString(array $options = [])
     {
         if (($options['format'] ?? null) === 'exiftool') {
