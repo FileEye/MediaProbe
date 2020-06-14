83a39a41f1aa35cf5e2ebdec23fa64f6ead79853
mondrake <mondrake.org@gmail.com>
2020-06-14 18:18:56 +0200

    54

--- test/MediaFilesTest.php
+++ test/MediaFilesTest.php
@@ -172,16 +172,16 @@ class MediaFilesTest extends MediaProbeTestCaseBase
                         }
                     }
                     $this->assertNotNull($n, 'Exiftool raw missing: ' . $exiftool_node);
-                    $valx = $n->textContent;
+                    $valx = rtrim($n->textContent, " ");
                     $vala = $element->getValue(['format' => 'exiftool']);
 //if (($expected['class'] ?? null) === 'FileEye\MediaProbe\Entry\Time') {
-if ($element->getParentElement() && in_array($element->getParentElement()->getAttribute('name'), ['ImageDescription'])) {
+/*if ($element->getParentElement() && in_array($element->getParentElement()->getAttribute('name'), ['ImageDescription'])) {
   dump([
     'element_' => $element->getParentElement()->getAttribute('name'),
     'expected' => MediaProbe::dumpHexFormatted($valx),
     'actual__' => MediaProbe::dumpHexFormatted($vala),
   ]);
-}
+}*/
                     if (in_array($element->getFormat(), [ItemFormat::RATIONAL, ItemFormat::SIGNED_RATIONAL, ItemFormat::SHORT_RATIONAL, ItemFormat::SHORT_SIGNED_RATIONAL, ItemFormat::DOUBLE, ItemFormat::FLOAT])) {
                         $this->assertEqualsWithDelta((float) $valx, (float) $vala, 0.001, 'Exiftool raw: ' . $element->getContextPath());
                     } else {
