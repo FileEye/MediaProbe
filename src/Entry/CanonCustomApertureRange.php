da445c5ae38763e6f7f2029cfd08e74e8ab3c06b
mondrake <mondrake.org@gmail.com>
2020-06-16 21:58:27 +0200

    2

--- src/Entry/CanonCustomApertureRange.php
+++ src/Entry/CanonCustomApertureRange.php
@@ -5,9 +5,9 @@ namespace FileEye\MediaProbe\Entry;
 use FileEye\MediaProbe\Entry\Core\SignedLong;
 
 /**
- * Handler for CanonCustom tags representing Shutter speed range.
+ * Handler for CanonCustom tags representing Aperture range.
  */
-class CanonCustomShutterSpeedRange extends SignedLong
+class CanonCustomApertureRange extends SignedLong
 {
     /**
      * {@inheritdoc}
@@ -18,8 +18,8 @@ class CanonCustomShutterSpeedRange extends SignedLong
         if ($format === 'exiftool') {
             $v = [];
             $v[0] = $this->value[0];
-            $v[1] = exp(-($this->value[1] / 8 - 7) * log(2));
-            $v[2] = exp(-($this->value[2] / 8 - 7) * log(2));
+            $v[1] = exp(($this->value[1] / 8 - 1) * log(2) / 2);
+            $v[2] = exp(($this->value[2] / 8 - 1) * log(2) / 2);
             return implode(' ', $v);
         }
         return parent::getValue();
