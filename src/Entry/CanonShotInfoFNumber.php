fccd6d5d83c09929d0bab83eaf6a091416fd24c9
mondrake <mondrake.org@gmail.com>
2020-10-23 23:10:45 +0200

    f2

--- src/Entry/CanonShotInfoFNumber.php
+++ src/Entry/CanonShotInfoFNumber.php
@@ -2,55 +2,16 @@
 
 namespace FileEye\MediaProbe\Entry;
 
-use FileEye\MediaProbe\Entry\Core\SignedShort;
-use FileEye\MediaProbe\MediaProbe;
-
 /**
  * Handler for Canon ShotInfo FNumber tags.
  */
-class CanonShotInfoFNumber extends SignedShort
+class CanonShotInfoFNumber extends CanonApertureValue
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getValue(array $options = [])
-    {
-        $alternative = $this->getRootElement()->getElement("//makerNote[@name='Canon']/*[@name='CanonCameraInfo']/tag[@name='FNumber']/entry");
-/*        if ($alternative = $this->getRootElement()->getElement("//makerNote[@name='Canon']/*[@name='CanonCameraInfo']/tag[@name='FNumber']/entry")) {
-            return exp($this->CanonEv($alternative->getValue($options)) * log(2) / 2);
-        } else {
-            return exp($this->CanonEv($this->value[0]) * log(2) / 2);
-        }*/
-        $value = exp($this->CanonEv($this->value[0]) * log(2) / 2);
-//        dump([$this->value[0], $value, $alternative ? $alternative->getValue() : 'nope']);
-        return $value;
-    }
-
     /**
      * {@inheritdoc}
      */
     public function toString(array $options = [])
     {
         return round($this->getValue(), 1);
-    }
-
-    private function CanonEv($val) {
-        // temporarily make the number positive
-        if ($val < 0) {
-            $val = -$val;
-            $sign = -1;
-        } else {
-            $sign = 1;
-        }
-        $frac = $val & 0x1f;
-        $val -= $frac;
-        // remove fraction
-        // Convert 1/3 and 2/3 codes
-        if ($frac == 0x0c) {
-            $frac = 0x20 / 3;
-        } elseif ($frac == 0x14) {
-            $frac = 0x40 / 3;
-        }
-        return $sign * ($val + $frac) / 0x20;
     }
 }
