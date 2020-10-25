626aeda8be944b859d3221ecc611bdbd95a59d5b
mondrake <mondrake.org@gmail.com>
2020-10-25 23:38:52 +0100

    11

--- src/Block/Map.php
+++ src/Block/Map.php
@@ -71,24 +71,19 @@ class Map extends Index
     public function toBytes($byte_order = ConvertBytes::LITTLE_ENDIAN, $offset = 0, $has_next_ifd = false)
     {
         $data_bytes = $this->getElement("rawData[@name='mapdata']/entry")->getValue();
-if ($this->getAttribute('name') === 'CanonTimeInfo') dump($this->getAttribute('name'), 'fetch', MediaProbe::dumpHexFormatted($data_bytes));
+
         // Dump each tag at the position in the map specified by the item id.
         foreach ($this->getMultipleElements('*[not(self::rawData)]') as $sub_id => $sub) {
             $bytes_offset = $sub->getAttribute('id') * ItemFormat::getSize($this->getFormat());
-//if ($this->getAttribute('name') === 'CanonCameraSettings') dump($sub_id, 'bytes_offset', $bytes_offset);
             $bytes = $sub->toBytes($byte_order);
-//if ($this->getAttribute('name') === 'CanonCameraSettings') dump($sub_id, 'bytes', MediaProbe::dumpHexFormatted($bytes));
             $bytes_length = strlen($bytes);
 
             $tmp = substr($data_bytes, 0, $bytes_offset);
-//if ($this->getAttribute('name') === 'CanonCameraSettings') dump($sub_id, 'tmp_1', MediaProbe::dumpHexFormatted($tmp));
             $tmp .= $bytes;
             $tmp .= substr($data_bytes, $bytes_offset + $bytes_length);
-//if ($this->getAttribute('name') === 'CanonCameraSettings') dump($sub_id, 'tmp_3', MediaProbe::dumpHexFormatted($tmp));
 
             $data_bytes = $tmp;
         }
-if ($this->getAttribute('name') === 'CanonTimeInfo') dump($this->getAttribute('name'), 'save', MediaProbe::dumpHexFormatted($data_bytes));
 
         return $data_bytes;
     }
